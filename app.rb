e5f267dba984741d1100d2a346ab88206017352e
korey ko <koreykendrick1@gmail.com>
2020-07-04 06:17:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do 
+    "Hello, world!"
+  end
+
+end
 
 
 end
